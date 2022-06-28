25455b33e6bcc70e8c27a6a7b0402e7f6d337764
Michael Zingale <michael.zingale@stonybrook.edu>
2022-06-28 07:59:56 -0400

    fix css path

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -109,13 +109,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        '_static/css/theme.css',
-        '_static/pygments.css'
-        ],
-    }
+html_css_files = ["theme_overrides.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -185,6 +179,3 @@ texinfo_documents = [
      author, 'AMReX-Astro', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
